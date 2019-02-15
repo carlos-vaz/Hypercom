73a045bc2d65530ecc0dac89e9e5ee3611d5b8ef
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@fq-wifi-10-215-191-68.wireless.pitt.edu>
2019-02-15 14:07:31 -0500

    added native mpi collective comm

---
+++ HW2/native.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+
+#define MAX_PROCS 1000
+
+int myrank, np, per_proc;
+
+/* Compute single function value. 
+ * To swap function, modify this only. 
+ */
+double func_val(double x) {
+	return 4/(1+pow(x,2));
+}
+
+/* Store function values in array. 
+ * Only one special process will call
+ * this function, and pass the chunks
+ * to its friends. 
+ */
+void func_gen(double * arr, double X_min, double X_max, int Points) {
+	double x = X_min, Delta = (X_max-X_min)/Points;
+	for(int i=0; i<Points; i++, x += Delta)
+		arr[i] = func_val(x);
+}
+
+int main(int argc, char* argv[]) {
+	MPI_Status status;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+	struct timeval stop, start;
+
+	// Parse arguments
+	if(myrank==0 && argc!=4) {
+		printf("Abort. main needs 3 arguments: \"mpirun -np [# procs] main [# Points] [X_min] [X_max]\"\n");
+		MPI_Abort(MPI_COMM_WORLD, 16);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+	int Points = strtol(argv[1], NULL, 10);
+	double X_min = atof(argv[2]);
+	double X_max = atof(argv[3]);
+
+	// Check divisibility of labor
+	if(myrank==0 && (Points % np != 0 || Points == np) ) {
+		printf("Abort. Number of processes (%d) must divide number of points (%d), and " \
+		"cannot equal number of points.\n", np, Points);
+		MPI_Abort(MPI_COMM_WORLD, 17);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+	per_proc = Points/np;
+
+	// Propagate data down the tree using MPI natives
+	double * arr = NULL;
+	double * myshare = malloc(per_proc*sizeof(double));
+	if(myrank==0) {
+		gettimeofday(&start, NULL);
+		arr = malloc(Points*sizeof(double));
+		func_gen(arr, X_min, X_max, Points);
+	}
+	MPI_Scatter(arr, per_proc, MPI_DOUBLE, myshare, per_proc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	free(arr);
+
+	// Perform the integration
+	double sum=0;
+	double Delta = (X_max-X_min)/Points;
+	for(int i=0; i<per_proc-1; i++)
+		sum += (myshare[i]+myshare[i+1])*Delta/2;
+	free(myshare);
+
+
+	// Collect sums up the tree using MPI natives
+	double * sums = NULL;
+	if(myrank==0)
+		sums = malloc(np*sizeof(double));
+	MPI_Gather(&sum, 1, MPI_DOUBLE, sums, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	free(sums);
+
+	printf("SUMS: ");
+	for(int i=0; i<np; i++)
+		printf("%lf, ", sums[i]);
+	printf("\n");
+
+	for(int i=1; i<np; i++)
+		sum += sums[i];
+
+	// Print results
+	if(myrank==0) {
+		gettimeofday(&stop, NULL);
+		printf("PROC %d REPORTS SUM = %lf", myrank, sum);
+		printf("\t<-- Result. ELAPSED TIME: %f sec", (double)(stop.tv_usec-start.tv_usec)/1000000 + stop.tv_sec-start.tv_sec);
+	}
+	printf("\n");
+
+	MPI_Finalize();
+	return 0;
+}
