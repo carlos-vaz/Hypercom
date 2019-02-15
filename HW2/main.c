7448cd4f4146352d520257542b6220dc70c47563
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@fq-wifi-10-215-190-248.wireless.pitt.edu>
2019-02-14 23:36:05 -0500

    fix

--- HW2/main.c
+++ HW2/main.c
@@ -87,7 +87,8 @@ int main(int argc, char* argv[]) {
 	// Check divisibility of labor
 	if(Points % np != 0) {
 		printf("Abort. Number of processes (%d) must divide number of points (%d).\n", np, Points);
-		printf("argv[1] = %d, argv[2] = %d, argv[3] = %d", *argv[1], *argv[2], *argv[3]);
+		printf("argv[1] = %d\n", *argv[1]);
+		//printf("argv[1] = %d, argv[2] = %d, argv[3] = %d\n", *argv[1]), *argv[2], *argv[3]);
 		goto finish;
 	}
 	per_proc = Points/np;
