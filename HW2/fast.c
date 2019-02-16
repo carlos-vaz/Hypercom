768599cbfa32b11a942905c710ea4d13e51fd6f8
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@fq-wifi-10-215-190-166.wireless.pitt.edu>
2019-02-15 19:49:35 -0500

    fast fix

--- HW2/fast.c
+++ HW2/fast.c
@@ -148,10 +148,11 @@ int main(int argc, char* argv[]) {
 
 	// Gather the sums into proc 0
 	double recv_sum=0;
-	int rank_decay=myrank;
+	int rank_decay = myrank;
 	for(int np_grow=1; np_grow!=np; np_grow<<=1, rank_decay>>=1) {
 		if(rank_decay % np_grow == 1) {
 			printf("LOOP: rank_decay = %d\n", rank_decay);
+			MPI_Barrier(MPI_COMM_WORLD);
 			MPI_Send(&sum, 1, MPI_DOUBLE, myrank-np_grow, 5, MPI_COMM_WORLD);
 			break; // Whoever you sent to now represents your group. you leave. 
 		}
