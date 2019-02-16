54bd2f8aafce7064f57124a5553149f92264034f
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@fq-wifi-10-215-178-63.wireless.pitt.edu>
2019-02-16 15:30:30 -0500

    fast fix

--- HW2/fast.c
+++ HW2/fast.c
@@ -155,8 +155,8 @@ int main(int argc, char* argv[]) {
 			if(prev_offset != 0)
 				MPI_Send(snd_bf, snd_sz, MPI_DOUBLE, prev_offset, 7, MPI_COMM_WORLD);
 			arr_sz = virtual_points*per_proc;
-			while(propagate(arr, &arr_sz, myshare)==1) printf("if\n");
 		}
+		while(propagate(arr, &arr_sz, myshare)==1) printf("if\n");	
 	} else {
 		// Block until you receive a message, then receive and propagate down tree
 		printf("(%d) Entered Recving Else\n",myrank);
