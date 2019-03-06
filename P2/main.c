9a3a64781ad5bee94250800386cc47a63590ca4a
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@fq-wifi-10-215-180-214.wireless.pitt.edu>
2019-03-06 15:40:06 -0500

    main.c

--- P2/main.c
+++ P2/main.c
@@ -20,12 +20,12 @@ int main(int argc, char* argv[]) {
 	periodic[0] = periodic[1] = 0;				// what is periodic?
 	int dimensions[2];
 	dimensions[0] = 10; dimensions[1] = 10;			// 10 x 10?
-	int coors_2d[2];
+	int coord_2d[2];
 	int rank_2d;
-	MPI_Cart_create(comm,ndim,dimensions,periodic,1,&comm2d);
+	MPI_Cart_create(MPI_COMM_WORLD,ndim,dimensions,periodic,1,&comm2d);
 	MPI_Cart_coords(comm2d,0,ndim,coord_2d);
 	MPI_Cart_rank(comm2d,coord_2d,&rank_2d);
-	printf("I am %d: (%d,%d); originally %d\n",rank_2d,coord_2d[0],coord_2d[1],procno);
+	printf("I am %d: (%d,%d); originally %d\n",rank_2d,coord_2d[0],coord_2d[1],0);
 
 
 	return 0;
