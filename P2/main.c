71189127c4564e0026be8201e9d06c705063f0ad
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@fq-wifi-10-215-180-214.wireless.pitt.edu>
2019-03-06 15:46:08 -0500

    main.c

--- P2/main.c
+++ P2/main.c
@@ -23,9 +23,9 @@ int main(int argc, char* argv[]) {
 	int coord_2d[2];
 	int rank_2d;
 	MPI_Cart_create(MPI_COMM_WORLD,ndim,dimensions,periodic,1,&comm2d);
-	MPI_Cart_coords(comm2d,0,ndim,coord_2d);
+	MPI_Cart_coords(comm2d,myrank,ndim,coord_2d);
 	MPI_Cart_rank(comm2d,coord_2d,&rank_2d);
-	printf("I am %d: (%d,%d); originally %d\n",rank_2d,coord_2d[0],coord_2d[1],0);
+	printf("I am %d: (%d,%d); originally %d\n",rank_2d,coord_2d[0],coord_2d[1],myrank);
 
 
 	return 0;
