5ab7c81bef79d6e3f9b0d7fcb2e035a110b575e2
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-103-190.wireless.pitt.edu>
2019-03-07 14:02:17 -0500

    wd

--- P2/writedata.c
+++ P2/writedata.c
@@ -10,14 +10,14 @@
 
 int main(int argc, char* argv[]) {
 	if(argc!=3) {
-		printf("ERROR! Format: ./writedata [# points x] [# points y]\n");
+		printf("ERROR! Format: ./writedata [# points y] [# points x]\n");
 		exit(0);
 	}
 
-	int x_dim = atoi(argv[1]);
-	int y_dim = atoi(argv[2]);
+	int x_dim = atoi(argv[2]);
+	int y_dim = atoi(argv[1]);
 	char * filename = malloc(20);
-	sprintf(filename, "data_%dx%d.txt", x_dim, y_dim);
+	sprintf(filename, "data_%dx%d.txt", y_dim, x_dim);
 	printf("x_dim=%d\ny_dim=%d\nWriting data to %s\n",x_dim, y_dim, filename);
 	int fd = open(filename, O_WRONLY | O_CREAT, 0644, 1);
 
