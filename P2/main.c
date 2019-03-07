40d70b5d7ff55b4cf18421a883e92272df5f8f19
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-103-190.wireless.pitt.edu>
2019-03-07 15:19:08 -0500

    main

--- P2/main.c
+++ P2/main.c
@@ -113,22 +113,22 @@ int main(int argc, char* argv[]) {
 		printf("Process (%d, %d): I have a southern boundary\n", mycoord[0], mycoord[1]);
 		// Do nothing (xe^y == 0)
 	}
-/*	if(mycoord[1]==dims_procs[1]) {
+	if(mycoord[1]==dims_procs[1]) {
 		printf("Process (%d, %d): I have a northern boundary\n", mycoord[0], mycoord[1]);
-		for(int x=0; x<dims_pts[0]; x++)
+		for(int x=0; x<proc_pts[0]; x++)
 			T[(proc_pts[1]-1)*proc_pts[0]+x] = v[(proc_pts[1]-1)*proc_pts[0]+x]; // copy from 'v'
 	}
-*/	if(mycoord[0]==0) {
+	if(mycoord[0]==0) {
 		printf("Process (%d, %d): I have an eastern boundary\n", mycoord[0], mycoord[1]);
-		for(int y=0; y<dims_pts[1]; y++)
+		for(int y=0; y<proc_pts[1]; y++)
 			T[y*proc_pts[0]] = v[y*proc_pts[0]]; // copy from 'v'
 	}
-/*	if(mycoord[0]==dims_procs[0]) {
+	if(mycoord[0]==dims_procs[0]) {
 		printf("Process (%d, %d): I have a western boundary\n", mycoord[0], mycoord[1]);
-		for(int y=0; y<dims_pts[1]; y++)
+		for(int y=0; y<proc_pts[1]; y++)
 			T[(y+1)*proc_pts[0]-1] = v[(y+1)*proc_pts[0]-1];  // copy from 'v'
 	}
-*/
+
 	MPI_Finalize();
 	return 0;
 }
