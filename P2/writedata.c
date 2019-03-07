376ff3c6378b559e952e2111c5330b44536e8f12
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-103-190.wireless.pitt.edu>
2019-03-07 14:15:10 -0500

    wd

--- P2/writedata.c
+++ P2/writedata.c
@@ -15,7 +15,7 @@
 
 int main(int argc, char* argv[]) {
 	if(argc!=5) {
-		printf("ERROR! Format: ./writedata [Y Range] [X Range] [# points y] [# points x]\n");
+		printf("USAGE INFO:\n\t./writedata [Y Range] [X Range] [# points y] [# points x]\n");
 		exit(0);
 	}
 
