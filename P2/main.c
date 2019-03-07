d889a92d7fefdf520ad7dd74b3648c378c8d16c3
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-103-190.wireless.pitt.edu>
2019-03-07 14:01:57 -0500

    main

--- P2/main.c
+++ P2/main.c
@@ -21,7 +21,9 @@ int main(int argc, char* argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-	// Check for correct number of arguments
+	/*
+	 * Check for correct number of arguments
+	 */
 	if(argc!=4) {
 		if(myrank==0)
 			printf("ERROR: arguments to main: mpirun -np [#procs] main [sourcefile] [# procs X] [# procs Y] \n");
@@ -31,14 +33,15 @@ int main(int argc, char* argv[]) {
 
 	/*
 	 * Read the file metadata and check for do-ablility
+	 * File metadata in the form: X dimensions, Y dimensions
 	 */
 	dims_procs[0] = atoi(argv[2]);
 	dims_procs[1] = atoi(argv[3]);
-	printf("Will solve %s with %d (y) by %d (x) processes\n", argv[1], dims_procs[0], dims_procs[1]);
+	printf("Will solve %s with %d (x) by %d (y) processes\n", argv[1], dims_procs[0], dims_procs[1]);
 	MPI_File file; 
 	MPI_File_open(MPI_COMM_WORLD, argv[1], MPI_MODE_RDONLY, MPI_INFO_NULL, &file);
 	MPI_File_read_all(file, &dims_pts, 2, MPI_INT, MPI_STATUS_IGNORE);
-	printf("FILE READ... DIMENSIONS: %d BY %d\n", dims_pts[0], dims_pts[1]);
+	printf("FILE READ... DIMENSIONS (MATRIX FORM): %d BY %d\n", dims_pts[1], dims_pts[0]);
 	if(dims_pts[0]%dims_procs[0]!=0 || dims_pts[1]%dims_procs[1]!=0) {
 		if(myrank == 0)
 			printf("Number of points must be divisible by number of procs in that dimension\n");
@@ -56,6 +59,7 @@ int main(int argc, char* argv[]) {
 	MPI_Cart_coords(comm2d,myrank, 2,mycoord);
 	MPI_Cart_rank(comm2d,mycoord,&rank_2d);
 	printf("I am %d: (%d,%d); originally %d\n",rank_2d,mycoord[0],mycoord[1],myrank);
+	
 
 
 	/*
@@ -66,13 +70,14 @@ int main(int argc, char* argv[]) {
 	proc_pts[1] = dims_pts[1]/dims_procs[1]; // # pts in X dim of process partition
 	proc_size = proc_pts[0]*proc_pts[1];
 	double *v = (double*)malloc(proc_size*sizeof(double));
-	printf("# blocks per process = %d\t# pts per proc (Y) = %d\t# pts per proc (X) = %d\n",proc_pts[0], proc_pts[0], proc_pts[1]);
+	printf("# blocks per process = %d\t# pts per proc (X) = %d\t# pts per proc (Y) = %d\n",proc_pts[1], proc_pts[0], proc_pts[1]);
 	printf("proc_size = %d\n",proc_size);
-	MPI_Type_vector(proc_pts[0], proc_pts[1], proc_pts[1]*np, MPI_DOUBLE, &vector);
+	MPI_Type_vector(proc_pts[1], proc_pts[0], proc_pts[0]*dims_procs[0], MPI_DOUBLE, &vector);
 	MPI_Type_commit(&vector);	
 	printf("MPI_File_set_view(file, 2*sizeof(int)+%d*%d+%d*%d, MPI_DOUBLE, vector, NULL, MPI_INFO_NULL)\n", \
-	mycoord[0], proc_pts[1] ,mycoord[1], dims_pts[1]);
-	MPI_File_set_view(file, 2*sizeof(int)+(mycoord[1]*proc_pts[1]+mycoord[0]*dims_pts[1])*sizeof(double), MPI_DOUBLE, vector, "native", MPI_INFO_NULL);	
+								mycoord[0], proc_pts[1] ,mycoord[1], dims_pts[1]);
+	MPI_File_set_view(file, 2*sizeof(int)+(mycoord[1]*proc_pts[1]+mycoord[0]*proc_size)*sizeof(double), \
+								MPI_DOUBLE, vector, "native", MPI_INFO_NULL);	
 	MPI_File_read_all(file, v, proc_size, MPI_DOUBLE, MPI_STATUS_IGNORE);
 
 	fflush(stdout);
@@ -89,6 +94,18 @@ int main(int argc, char* argv[]) {
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
+
+	/*
+	 * Each process creates a temperature vector T and fills it with 
+	 * 0, except for the boundaries, which take the initial value 
+	 * of xe^y 
+	 */
+/*	double *T = (double*)calloc(proc_size, sizeof(double));
+	for(int i=0; i<dims_pts[1]; i++) {
+		T[i] = 0;
+		
+	}
+*/	
 
 	MPI_Finalize();
 	return 0;
