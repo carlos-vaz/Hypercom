38e44215a8ba112d8f5f0ea244891342989b0ce8
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-103-190.wireless.pitt.edu>
2019-03-07 14:20:33 -0500

    main

--- P2/main.c
+++ P2/main.c
@@ -76,7 +76,7 @@ int main(int argc, char* argv[]) {
 	MPI_Type_vector(proc_pts[1], proc_pts[0], proc_pts[0]*dims_procs[0], MPI_DOUBLE, &vector);
 	MPI_Type_commit(&vector);	
 	printf("MPI_File_set_view(file, 2*sizeof(int)+%d*%d+%d*%d, MPI_DOUBLE, vector, NULL, MPI_INFO_NULL)\n", \
-								mycoord[0], proc_pts[1] ,mycoord[1], dims_pts[1]);
+								mycoord[1], proc_pts[1] ,mycoord[0], proc_size);
 	MPI_File_set_view(file, 2*sizeof(int)+(mycoord[1]*proc_pts[1]+mycoord[0]*proc_size)*sizeof(double), \
 								MPI_DOUBLE, vector, "native", MPI_INFO_NULL);	
 	MPI_File_read_all(file, v, proc_size, MPI_DOUBLE, MPI_STATUS_IGNORE);
