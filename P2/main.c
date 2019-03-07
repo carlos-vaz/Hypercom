9ada6044a1edd6c9303d620242296d6842505ab3
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-103-190.wireless.pitt.edu>
2019-03-07 12:57:44 -0500

    main

--- P2/main.c
+++ P2/main.c
@@ -68,7 +68,9 @@ int main(int argc, char* argv[]) {
 	double *v = (double*)malloc(proc_size*sizeof(double));
 	printf("# blocks per process = %d\t# pts per proc (X) = %d\t# pts per proc (Y) = %d\n",proc_pts[0], proc_pts[1], proc_pts[0]);
 	MPI_Type_vector(proc_pts[1], proc_pts[0], proc_pts[1]*np, MPI_DOUBLE, &vector);
-	MPI_Type_commit(&vector);
+	MPI_Type_commit(&vector);	
+	printf("MPI_File_set_view(file, 2*sizeof(int)+%d*%d+%d*%d, MPI_DOUBLE, vector, NULL, MPI_INFO_NULL)\n", \
+	mycoord[0], proc_pts[1] ,mycoord[1], dims_pts[1]);
 	MPI_File_set_view(file, 2*sizeof(int)+mycoord[0]*proc_pts[1]+mycoord[1]*dims_pts[1], MPI_DOUBLE, vector, NULL, MPI_INFO_NULL);	
 	MPI_File_read_all(file, v, proc_size, MPI_DOUBLE, MPI_STATUS_IGNORE);
 
