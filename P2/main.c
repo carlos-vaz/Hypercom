cc9d65496a09a5732187bde0fe1f17faca070e57
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-103-190.wireless.pitt.edu>
2019-03-07 11:09:02 -0500

    main

--- P2/main.c
+++ P2/main.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-int myrank, np, x_proc, y_proc, num_points, dimensions_pts[2];
+int myrank, rank_2d, coord_2d[2], np, dims_procs[2], num_points, dims_pts[2];
 
 /*void read_file_into_vector(MPI_File *file, void *buf, int count, MPI_Dataype vector, int int_or_double) {
 	MPI_File_read_all(*file, buf, count, MPI_
@@ -22,9 +22,9 @@ int main(int argc, char* argv[]) {
 		MPI_Abort(MPI_COMM_WORLD, -1);
 	}
 
-	x_proc = atoi(argv[2]);
-	y_proc = atoi(argv[3]);
-	printf("Will solve %s with %d horizontal processes and %d vertical processes\n", argv[1], x_proc, y_proc);
+	dims_procs[0] = atoi(argv[2]);
+	dims_procs[1] = atoi(argv[3]);
+	printf("Will solve %s with %d horizontal processes and %d vertical processes\n", argv[1], dims_procs[0], dims_procs[1]);
 
 	MPI_File file; 
 	MPI_File_open(MPI_COMM_WORLD, argv[1], MPI_MODE_RDONLY, MPI_INFO_NULL, &file);
@@ -37,13 +37,8 @@ int main(int argc, char* argv[]) {
 	//MPI_Vector_type();
 
 	MPI_Comm comm2d;
-	int ndim = 2;
 	int periodic[2] = {0,0};	// What is periodic?
-	int dimensions[2];
-	dimensions[0] = 1; dimensions[1] = 5;
-	int coord_2d[2];
-	int rank_2d;
-	MPI_Cart_create(MPI_COMM_WORLD,ndim,dimensions,periodic,1,&comm2d);
+	MPI_Cart_create(MPI_COMM_WORLD,2,dims_procs,periodic,1,&comm2d);
 	MPI_Cart_coords(comm2d,myrank,ndim,coord_2d);
 	MPI_Cart_rank(comm2d,coord_2d,&rank_2d);
 	printf("I am %d: (%d,%d); originally %d\n",rank_2d,coord_2d[0],coord_2d[1],myrank);
