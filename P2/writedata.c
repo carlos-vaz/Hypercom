fb12ed193b2d2bc8f02e025e0cdecd61129d0695
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-103-190.wireless.pitt.edu>
2019-03-07 10:43:11 -0500

    wridedata

--- P2/writedata.c
+++ P2/writedata.c
@@ -15,8 +15,10 @@ int main(int argc, char* argv[]) {
 
 	int x_dim = atoi(argv[1]);
 	int y_dim = atoi(argv[2]);
-	printf("x_dim=%d\ny_dim=%d\nWriting data to data_%d_%d.txt\n",x_dim, y_dim, x_dim, y_dim);
-	int fd = open("./data_%d.txt", O_WRONLY | O_CREAT, 0644, 1);
+	char * filename = malloc(20);
+	filename = sprintf(filename, "data_%dx%d.txt", x_dim, y_dim);
+	printf("x_dim=%d\ny_dim=%d\nWriting data to %s",x_dim, y_dim, filename);
+	int fd = fopen(filename, O_WRONLY | O_CREAT, 0644, 1);
 	char * test = "hello\n";
 	write(fd, test, 6);
 	close(fd);
