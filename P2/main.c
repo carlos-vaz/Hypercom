d2e256a0563d3459d68ebb810e5d9baa02926e44
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@Carloss-MacBook-Air-2.local>
2019-03-09 14:54:18 -0500

    main

--- P2/main.c
+++ P2/main.c
@@ -231,7 +231,7 @@ int main(int argc, char* argv[]) {
 			for(x=1; x<proc_pts[0]-1; x++) {
 				i = index(x,0);
 				T[i] = (-1*v[i]*pow((deltas[0]*deltas[1]),2)+(T[left(i)]+T[right(i)])*pow(deltas[1],2)+ \
-						(recv_south[i]+T[up(i)])*pow(deltas[0],2))/(2*pow(deltas[0],2)+2*pow(deltas[1],2));
+						(recv_south[x]+T[up(i)])*pow(deltas[0],2))/(2*pow(deltas[0],2)+2*pow(deltas[1],2));
 			}
 		}
 /*		if(got_south==1 && got_east==1) {
@@ -247,7 +247,7 @@ int main(int argc, char* argv[]) {
 */		for(y=1; y<proc_pts[1]-1; y++) {
 			if(got_west) {
 				i = index(0,y);
-				T[i] = (-1*v[i]*pow((deltas[0]*deltas[1]),2)+(recv_west[i]+T[right(i)])*pow(deltas[1],2)+ \
+				T[i] = (-1*v[i]*pow((deltas[0]*deltas[1]),2)+(recv_west[y]+T[right(i)])*pow(deltas[1],2)+ \
 						(T[down(i)]+T[up(i)])*pow(deltas[0],2))/(2*pow(deltas[0],2)+2*pow(deltas[1],2));
 			}
 			for(x=1; x<proc_pts[0]-1; x++) {
@@ -257,7 +257,7 @@ int main(int argc, char* argv[]) {
 			}
 			if(got_east) {
 				i = index(proc_pts[0]-1, y);
-				T[i] = (-1*v[i]*pow((deltas[0]*deltas[1]),2)+(T[left(i)]+recv_east[i])*pow(deltas[1],2)+ \
+				T[i] = (-1*v[i]*pow((deltas[0]*deltas[1]),2)+(T[left(i)]+recv_east[y])*pow(deltas[1],2)+ \
 						(T[down(i)]+T[up(i)])*pow(deltas[0],2))/(2*pow(deltas[0],2)+2*pow(deltas[1],2));
 			}
 		}
@@ -265,7 +265,7 @@ int main(int argc, char* argv[]) {
 			for(x=1; x<proc_pts[0]-1; x++) {
 				i = index(x,proc_pts[1]-1);
 				T[i] = (-1*v[i]*pow((deltas[0]*deltas[1]),2)+(T[left(i)]+T[right(i)])*pow(deltas[1],2)+ \
-						(T[down(i)]+recv_north[i])*pow(deltas[0],2))/(2*pow(deltas[0],2)+2*pow(deltas[1],2));
+						(T[down(i)]+recv_north[x])*pow(deltas[0],2))/(2*pow(deltas[0],2)+2*pow(deltas[1],2));
 			}
 		}
 
