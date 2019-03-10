18d3c489f536f8de8e963ff30ad12ad93f7a6102
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@Carloss-MacBook-Air-2.local>
2019-03-09 19:25:05 -0500

    main

--- P2/main.c
+++ P2/main.c
@@ -246,7 +246,8 @@ int main(int argc, char* argv[]) {
 		}
 */		if(got_south==1) {
 			for(x=1; x<proc_pts[0]-1; x++) {
-				i = index(x,0);
+				//i = index(x,0);
+				i = index(x,proc_pts[1]-1);
 				//printf("(%d): v[%d] = %lf\n", myrank, i, v[i]);
 				T[i] = (-1*v[i]*pow((deltas[0]*deltas[1]),2)+(T[left(i)]+T[right(i)])*pow(deltas[1],2)+ \
 					(recv_south[x]+T[up(i)])*pow(deltas[0],2))/(2*pow(deltas[0],2)+2*pow(deltas[1],2));
@@ -281,7 +282,8 @@ int main(int argc, char* argv[]) {
 		}
 */		if(got_north==1) {
 			for(x=1; x<proc_pts[0]-1; x++) {
-				i = index(x,proc_pts[1]-1);
+				//i = index(x,proc_pts[1]-1);
+				i = index(x,0);
 				T[i] = (-1*v[i]*pow((deltas[0]*deltas[1]),2)+(T[left(i)]+T[right(i)])*pow(deltas[1],2)+ \
 				(T[down(i)]+recv_north[x])*pow(deltas[0],2))/(2*pow(deltas[0],2)+2*pow(deltas[1],2));
 			}
