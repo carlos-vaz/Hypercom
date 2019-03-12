e2075fead47b379ddac9ea68f588c60d296608de
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@Carloss-Air-2.attlocal.net>
2019-03-11 20:41:38 -0400

    main

--- P2/main.c
+++ P2/main.c
@@ -396,10 +396,10 @@ int main(int argc, char* argv[]) {
 		if(count%1000==0) {
 			memcpy(test_buffer, T, proc_size*sizeof(double));
 		}
-		if(count%1000==1 && verify_error(T, test_buffer, proc_size)==1) {
+/*		if(count%1000==1 && verify_error(T, test_buffer, proc_size)==1) {
 			will_break = 1;
 		}
-
+*/
 		double err=-1;
 		if(count%1000==1) {
 			err = get_error(T, test_buffer, proc_size);
