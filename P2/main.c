6c0d7820324189974a4fd65ededfe1931f972489
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@carloss-air-2.wireless.ad.nova.edu>
2019-03-13 23:22:56 -0400

    main

--- P2/main.c
+++ P2/main.c
@@ -494,6 +494,7 @@ int main(int argc, char* argv[]) {
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	if(myrank==ANNOUNCER_PROC) printf("\n\nElapsed time for %d procs = %lf seconds\n", np, t_stop - t_start);
+	if(myrank==ANNOUNCER_PROC) printf("\n\nNumber of cycles for %d procs = %d\n", np, count);
 
 	MPI_Finalize();
 	return 0;
