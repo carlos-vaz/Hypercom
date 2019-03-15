af227a102e3806b2fb9513c77179aeb22d2f4368
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@Carloss-Air-2.attlocal.net>
2019-03-15 12:59:05 -0400

    main

--- P2/main.c
+++ P2/main.c
@@ -455,7 +455,7 @@ int main(int argc, char* argv[]) {
 			MPI_Send(&max, 1, MPI_DOUBLE, 0, 10, comm2d);
 		}
 		if(myrank==0)
-			printf("(%d): MAX ABSOLUTE error: iteration %d... \t%.10e\n", myrank, count, max);
+			printf("(%d, pts=%dx%d): MAX ABSOLUTE error: iteration %d... \t%.10e\n", myrank, proc_pts[0], proc_pts[1], count, max);
 
 
 
