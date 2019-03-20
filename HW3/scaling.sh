3fd521c699f565d2dfaefb7c17bccdb5d460b147
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-102-204.wireless.pitt.edu>
2019-03-20 15:16:28 -0400

    strong scaling analysis

--- HW3/scaling.sh
+++ HW3/scaling.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo > log.out
-echo "Testing on 400000000 points with 1, 2, 4, 8, 16, 32, 64 threads...\n"
+echo "Testing on 400000000 points with 1, 2, 4, 8, 16, 32, 64 threads...
+"
 for i in 1 2 4 8 16 32 64
 do
 	./main 400000000 $i >> log.out
