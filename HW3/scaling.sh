e9b66e45509024da0752cff761ea99383ee02a75
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@cl2-wifi-10-215-16-81.wireless.pitt.edu>
2019-03-20 13:22:12 -0400

    strong scaling analysis

--- HW3/scaling.sh
+++ HW3/scaling.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in 1 2 4 8 16 32 64
+for i in {1 2 4 8 16 32 64}
 do
 	./main 4000000 $i >> log.out
 done
@@ -8,11 +8,11 @@ done
 echo "Timings:
 ____________________
 "
-grep log.out -e "ELAPSED" | awk -F": " '{print $2}' 
+grep log.out -e "ELAPSED" | awk -F": " '{print $i threads: $2}' 
 
 echo "
 Values:
 ____________________
 "
-grep log.out -e "FINAL" | awk -F": " '{print $2}' 
+grep log.out -e "FINAL" | awk -F": " '{print $i threads: $2}' 
 
