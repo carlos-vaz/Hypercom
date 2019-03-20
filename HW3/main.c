b12a235e6fb0b7f78f0cccec04ce58241d204427
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-97-120.wireless.pitt.edu>
2019-03-20 09:07:09 -0400

    main

--- HW3/main.c
+++ HW3/main.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 	 */
 	double *buf = malloc(np*sizeof(double));
 	double x;
-	for(int i=0; i<pts; i++) {
+	for(int i=0; i<np; i++) {
 		buf[i] = function( i/np );
 		printf("buf[%d] = %lf\n",i, buf[i]);
 	}
