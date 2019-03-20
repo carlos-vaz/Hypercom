ab892c221ce6905f42ef1045fbc6b528925f5bff
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-97-120.wireless.pitt.edu>
2019-03-20 08:58:51 -0400

    main

--- HW3/main.c
+++ HW3/main.c
@@ -4,8 +4,10 @@
 int nt, np;
 
 int main(int argc, char *argv[]) {
-	if(argc!=3)
-		printf("Usage:\n\t./main [# points] [# threads]");
+	if(argc!=3) {
+		printf("Usage:\n\t./main [# points] [# threads]\n");
+		exit(0);
+	}
 	np = atoi(argv[1]);
 	nt = atoi(argv[2]);
 	printf("Running %d threads on %d points\n", nt, np);
