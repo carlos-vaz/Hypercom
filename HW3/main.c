307d1444bef7cf40b028c84dd89428ff5db8223d
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@cl2-wifi-10-215-16-81.wireless.pitt.edu>
2019-03-20 12:44:33 -0400

    main

--- HW3/main.c
+++ HW3/main.c
@@ -87,7 +87,7 @@ int main(int argc, char *argv[]) {
 	 */
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	elapsed = end.tv_sec - start.tv_sec;
-	elapsed += (end.tv_nsec - start.tv_nsec)/1000000000;
+	elapsed += (end.tv_nsec - start.tv_nsec)/1000000000.00;
 	printf("ELAPSED TIME: %lf\n", elapsed);
 
 	return 0;
