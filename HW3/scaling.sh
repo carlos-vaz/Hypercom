a72a6278f6a5b33ef126db1e544f39b1d1b80f5e
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@cl2-wifi-10-215-16-81.wireless.pitt.edu>
2019-03-20 14:30:14 -0400

    strong scaling

--- HW3/scaling.sh
+++ HW3/scaling.sh
@@ -5,7 +5,10 @@ for i in {1 2 4 8 16 32 64}
 do
 	./main 40000000 $i >> log.out
 	let "n = $( (echo 'l('$i')/l(2)' | bc -l) | cut -c1-1)"
-	echo $n
+	printf "\xd"
+	printf "|"
+	printf "#%.0s" {1..$((i*3))}
+	printf " %.0s" {1..$(( (7-i)*3 ))}
 done
 
 echo "Timings:
