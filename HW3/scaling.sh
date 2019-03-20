4022bff5856a26ee416dfaf5d58674b72ced3011
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@cl2-wifi-10-215-16-81.wireless.pitt.edu>
2019-03-20 14:18:09 -0400

    strong

--- HW3/scaling.sh
+++ HW3/scaling.sh
@@ -3,7 +3,9 @@
 echo > log.out
 for i in {1 2 4 8 16 32 64}
 do
-	./main 400000000 $i >> log.out
+	./main 40000000 $i >> log.out
+	let "n = $( (echo 'l($i)/l(2)' | bc -l) | cut -c1-1)"
+	echo $n
 done
 
 echo "Timings:
