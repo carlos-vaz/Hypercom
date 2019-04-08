bc9673198140c001a5cf6097bece04c2ab0e63bf
Carlos Daniel Vazquez-Gomez Bertelli <crlsvzqz5@gmail.com>
2019-04-07 21:19:27 -0400

    main

--- P3/main.c
+++ P3/main.c
@@ -62,9 +62,8 @@ int main(int argc, char** argv) {
 			for(i=0; i<grid_size; i++) {
 				printf("\tSubroutine from thread %d (i=%d, Px = %d)\n", id, i, Px);
 				if(i/Px!=0 && i/Px!=Py-1 && i%Px!=0 && i%Px!=Px-1) {
-				//	T[i] = (-1*S[i]*pow(((double)(XRANGE/Px)*(double)(YRANGE/Py)),2)+(T[i-1]+T[i+1])*pow((double)(YRANGE/Py),2)+ \
-					(T[i-Px]+T[i+Px])*pow((double)(XRANGE/Px),2))/(2*pow((double)(XRANGE/Px),2)+2*pow((double)(YRANGE/Py),2));
-					T[i] = -7;
+					T[i] = (-1*S[i]*pow(((double)(XRANGE/Px)*(double)(YRANGE/Py)),2)+(T[i-1]+T[i+1])*pow((double)(YRANGE/Py),2)+ \
+					(T[i-Px]+T[i+Px])*pow((double)(XRANGE/Px),2))///(2*pow((double)(XRANGE/Px),2)+2*pow((double)(YRANGE/Py),2));
 
 				}
 			}
