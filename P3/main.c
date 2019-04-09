83162618871a388a343a770e11dca2877b4744a5
Carlos Daniel Vazquez-Gomez Bertelli <crlsvzqz5@gmail.com>
2019-04-08 20:03:09 -0400

    change to Jacobi

--- P3/main.c
+++ P3/main.c
@@ -1,67 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include <omp.h>
 
 #define XRANGE 1
 #define YRANGE 1
 
+#define TOLERANCE 1e-12
+
 int Px, Py, Tx, Ty, Ptx, Pty, shared_pts;
 long grid_size;
+double conv_error;
 
 
 extern 
 void VTK_out(const int N, const int M, const double *Xmin, const double *Xmax,
              const double *Ymin, const double *Ymax, const double *T,
              const int index);
-/*
- * If ran with the index of a shared point (thread-to-thread boundary point) as input, output is 
- * index of the corresponding mutex. 
- */
-/*int mutex_map(int i) {
-	int map;
-	int lX = i%Ptx ? ((i%Px)/Ptx)-1 : (((i+1)%Px)/Ptx)-1;
-	int lY = (i/Px)%Pty ? ((i/Px)/Pty)-1 : (((i/Px)+1)/Pty)-1;
-
-	if(i%Ptx==0 || i%Ptx==Ptx-1) {
-		// Lies on a Vertical boudnary
-		map = lX*(Py-2-lY) + i/Px - lY;
-		return map;
-	}
-	if((i/Px)%Pty==0 || (i/Px)%Pty==Pty-1) {
-		// Lies on a Horizontal boudnary
-		map = (Tx-1)*(Py-2) lX*(Py-2-lY) + i/Px;
-		return map;
-
-	}	
-}*/
-
-/*int mutex_map(int i, int id) {
-	int per_thread = 2*Ptx+2*Pty;
-	int map = per_thread*id;
-	int xt = i%Ptx;
-	int yt = (i/Px)%Pty;
-	if(xt==0 && yt==0) {
-		return map + 0;
-	}
-	if(xt==0 && yt==Pty-1) {
-		return map + 0;
-	}
-	
-}*/
-
-int mutex_map(int i, int id) {
-	int ret = 1;
-	if(i%Ptx==0)
-		ret *= (id - 1);
-	if(i%Ptx==Ptx-1)
-		ret *= (id + 1);
-	if((i/Px)%Pty==0)
-		ret *= (id - Tx);
-	if((i/Px)%Pty==0)
-		ret *= (id + Tx);
-	return ret;
-}
 
 void print_grid(double * G, long size, int sizex) {
 	for(int i=0; i<size; i++) {
@@ -83,10 +39,8 @@ int main(int argc, char** argv) {
 	grid_size = (long)Px*(long)Py; 
 	Ptx = Px/Tx;
 	Pty = Py/Ty;
+	int per_thread = Ptx*Pty;
 	shared_pts = (Tx-1)*(Py-2) + (Ty-1)*(Px-2) - 4*(Tx-1)*(Ty-1);
-      //omp_lock_t * mutexes = malloc(shared_pts*sizeof(omp_lock_t)); Lock array of minimum size, but hard to code
-	omp_lock_t * mutexes = malloc(Tx*Ty*sizeof(omp_lock_t));
-	for(int i=0; i<shared_pts; i++) omp_init_lock(&mutexes[i]);
 	if((Px%Tx!=0) || (Py%Ty!=0)) {
 		printf("Threads in X dimension must divide Points in X dimension, and likewise for Y dimension\n");
 		exit(-2);
@@ -96,42 +50,67 @@ int main(int argc, char** argv) {
 	// Make Grids
 	double * S = (double * )malloc(grid_size*sizeof(double));
 	double * T = (double * )calloc(grid_size, sizeof(double));
+	double * T_tmp = (double * )calloc(grid_size, sizeof(double));
 	for(long i=0; i<grid_size; i++) {
 		double val = (i%Px)*((double)XRANGE/Px)*powf(2.718281828, (i/Px)*((double)YRANGE/Py));
 		printf("TEST: %ld/%d = %lf\n", i, Px, (double)(i/Px));
 		S[i] = val;
-		if(i/Px==0 || i/Px==Py-1 || i%Px==0 || i%Px==Px-1)
+		if(i/Px==0 || i/Px==Py-1 || i%Px==0 || i%Px==Px-1) {
 			T[i] = val;
+			T_tmp[i] = val;
+		}
 	}
 
 	print_grid(T, grid_size, Px);
 
 	omp_set_num_threads(Tx*Ty);
 	int id, i, unlock=0;
-	#pragma omp parallel default(none) shared(S,T,grid_size,Tx,Ty,Px,Py,Ptx,Pty,mutexes) private(id,i,unlock)
-	{
-		id = omp_get_thread_num();
-		printf("Hello from thread %d\n", id);
+	int count = 0;
+	double conv_error, mymax = TOLERANCE+1;
+	while(conv_error < TOLERANCE) {
+		#pragma omp parallel default(none) shared(S,T,T_tmp,grid_size,Tx,Ty,Px,Py,Ptx,Pty,conv_error,count,per_thread) private(id,i,unlock,mymax)
+		{
+			id = omp_get_thread_num();
+			printf("Hello from thread %d\n", id);
 
-		#pragma omp for
-			for(i=0; i<grid_size; i++) {
-				printf("\tSubroutine from thread %d (i=%d, Px = %d)\n", id, i, Px);
-				if(i/Px!=0 && i/Px!=Py-1 && i%Px!=0 && i%Px!=Px-1) {
-					if(i%Ptx==Ptx-1 || i%Ptx==0 || (i/Px)%Pty==Pty-1 || (i/Px)%Pty==0) {
-						// Updating a shared point. Must lock!
-						omp_set_lock(&mutexes[mutex_map(i, id)]);
-						unlock = 1;
+			#pragma omp for
+				for(i=0; i<grid_size; i++) {
+					printf("\tSubroutine from thread %d (i=%d, Px = %d)\n", id, i, Px);
+					if(i/Px!=0 && i/Px!=Py-1 && i%Px!=0 && i%Px!=Px-1) {
+						T_tmp[i] = (-1*S[i]*pow((((double)XRANGE/Px)*((double)YRANGE/Py)),2)+(T[i-1]+T[i+1])*pow(((double)YRANGE/Py),2)+ \
+						(T[i-Px]+T[i+Px])*pow(((double)XRANGE/Px),2))/(2*pow(((double)XRANGE/Px),2)+2*pow(((double)YRANGE/Py),2));
 					}
-					T[i] = (-1*S[i]*pow((((double)XRANGE/Px)*((double)YRANGE/Py)),2)+(T[i-1]+T[i+1])*pow(((double)YRANGE/Py),2)+ \
-					(T[i-Px]+T[i+Px])*pow(((double)XRANGE/Px),2))/(2*pow(((double)XRANGE/Px),2)+2*pow(((double)YRANGE/Py),2));
-					if(unlock == 1) {
-						omp_unset_lock(&mutexes[mutex_map(i, id)]);
-						unlock = 0;
-					}
+				}
 
+			if(count%1000==0) {
+			#pragma omp for
+				for(i=0; i<grid_size; i++) {
+					if(i/Px!=0 && i/Px!=Py-1 && i%Px!=0 && i%Px!=Px-1) {
+						double candidate = fabs(T_tmp[i] - T[i]);
+						if(mymax < candidate)
+							mymax = candidate;
+					}
 				}
 			}
+
+			#pragma omp critical
+			{
+				if(mymax > conv_error)
+					conv_error = mymax;
+			}
+
+
+			/*#pragma omp for
+				for(i=0; i<grid_size; i++) {
+					if(i/Px!=0 && i/Px!=Py-1 && i%Px!=0 && i%Px!=Px-1)
+						T[i] = T_tmp[i];
+				}*/
+			memcpy(&T[id*per_thread], &T_tmp[id*per_thread], per_thread*sizeof(double));
+
+			
+		}
 	}
+
 
 	double Xmin = 0;
 	double Ymin = 0;
@@ -145,5 +124,6 @@ int main(int argc, char** argv) {
 
 	free(S);
 	free(T);
+	free(T_tmp);
 	return 0;
 }
