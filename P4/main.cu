9033cafd62ff9c1fbbea9dbdabeddc58d28e3e5a
cdv16 <crlsvzqz5@gmail.com>
2019-04-20 11:33:53 -0400

    main

--- P4/main.cu
+++ P4/main.cu
@@ -1,29 +1,54 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
+#include "math.h"
 
-int xdim, ydim;
+#define XRANGE 1
+#define YRANGE 1
+
+__global__
+void kernel(double * T, double * S) {
+	int id = blockIdx.x*blockDim.x + threadIdx.x;
+}
+
+int Px, Py, grid_size;
 
 int main(int argc, char **argv) {
 	if(argc!=3) {
 		printf("Usage:\n\t./main [xdim] [ydim]\n");
 		exit(0);
 	}
-	xdim = atoi(argv[1]);
-	ydim = atoi(argv[2]);
+	Px = atoi(argv[1]);
+	Py = atoi(argv[2]);
+	long grid_size = Px*Py;
 
-	// Make Grids
-	double * S = (double * )malloc(grid_size*sizeof(double));
-	double * T = (double * )calloc(grid_size, sizeof(double));
-	double * T_tmp = (double * )calloc(grid_size, sizeof(double));
+	// Allocate Grids (pinned memory)
+	double * h_S;
+	cudaMallocHost((void**)&h_S, grid_size*sizeof(double));
+	double * h_T;
+	cudaMallocHost((void**)&h_T, grid_size*sizeof(double));
+	double * h_T_tmp;
+	cudaMallocHost((void**)&h_T_tmp, grid_size*sizeof(double));
 	for(long i=0; i<grid_size; i++) {
 		double val = (i%Px)*((double)XRANGE/Px)*powf(2.718281828, (i/Px)*((double)YRANGE/Py));
-		//printf("TEST: %ld/%d = %lf\n", i, Px, (double)(i/Px));
-		S[i] = val;
+		h_S[i] = val;
 		if(i/Px==0 || i/Px==Py-1 || i%Px==0 || i%Px==Px-1) {
-			T[i] = val;
-			T_tmp[i] = val;
+			h_T[i] = val;
+			h_T_tmp[i] = val;
 		}
 	}
-	
+	double * d_S;
+	double * d_T;
+	double * d_T_tmp;
+	cudaMalloc((double**)&d_S, grid_size*sizeof(double));
+	cudaMalloc((double**)&d_T, grid_size*sizeof(double));	
+	cudaMalloc((double**)&d_T_tmp, grid_size*sizeof(double));
+	cudaMemcpy(d_S, h_S, grid_size*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_T, h_T_tmp, grid_size*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_T_tmp, h_T_tmp, grid_size*sizeof(double), cudaMemcpyHostToDevice);
+
+	int blocks = 1;
+	int threadsperblock = 1;
+	kernel<<<blocks,threadsperblock>>>(d_T,d_S);
+
 }
