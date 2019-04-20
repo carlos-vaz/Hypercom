7e70f29fd6c151068c8a92aba337036fded3f9b4
cdv16 <crlsvzqz5@gmail.com>
2019-04-20 17:28:55 -0400

    gpu fill initial grid

--- P4/main.cu
+++ P4/main.cu
@@ -7,9 +7,21 @@
 #define YRANGE 1
 
 __global__
-void kernel(double * T, double * S) {
+void kernel(double * T, double * T_tmp, double * S, int grid_size, int Px, int Py) {
 	int id = blockIdx.x*blockDim.x + threadIdx.x;
-	printf("Hello from thread %d\n", id);
+	if(id >= grid_size)
+		return;
+	double val = (id%Px)*((double)XRANGE/Px)*powf(2.718281828, (id/Px)*((double)YRANGE/Py));
+	S[id] = val;
+	T[id] = 0;
+	T_tmp[id] = 0;
+	if(id/Px!=0 && id/Px!=Py-1 && id%Px!=0 && id%Px!=Px-1)
+		return;
+	T[id] 	  = S[id];	
+	T_tmp[id] = S[id];
+	__syncthreads();
+
+
 }
 
 int Px, Py, grid_size;
@@ -23,38 +35,37 @@ int main(int argc, char **argv) {
 	Py = atoi(argv[2]);
 	long grid_size = Px*Py;
 
-	// Make initial grids (pinned memory)
+	// Allocate grids in Host to recieve from GPU (pinned memory)
 	double * h_S;
 	double * h_T;
 	double * h_T_tmp;
 	cudaMallocHost((void**)&h_S, grid_size*sizeof(double));
 	cudaMallocHost((void**)&h_T, grid_size*sizeof(double));
 	cudaMallocHost((void**)&h_T_tmp, grid_size*sizeof(double));
-	for(long i=0; i<grid_size; i++) {
-		double val = (i%Px)*((double)XRANGE/Px)*powf(2.718281828, (i/Px)*((double)YRANGE/Py));
-		h_S[i] = val;
-		if(i/Px==0 || i/Px==Py-1 || i%Px==0 || i%Px==Px-1) {
-			h_T[i] = val;
-			h_T_tmp[i] = val;
-		}
-	}
 	
-	// Transfer grids to GPU memory
+	// Allocate grids in GPU memory
 	double * d_S;
 	double * d_T;
 	double * d_T_tmp;
 	cudaMalloc((double**)&d_S, grid_size*sizeof(double));
 	cudaMalloc((double**)&d_T, grid_size*sizeof(double));	
 	cudaMalloc((double**)&d_T_tmp, grid_size*sizeof(double));
-	cudaMemcpy(d_S, h_S, grid_size*sizeof(double), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_T, h_T_tmp, grid_size*sizeof(double), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_T_tmp, h_T_tmp, grid_size*sizeof(double), cudaMemcpyHostToDevice);
+	//cudaMemcpy(d_S, h_S, grid_size*sizeof(double), cudaMemcpyHostToDevice);
+	//cudaMemcpy(d_T, h_T_tmp, grid_size*sizeof(double), cudaMemcpyHostToDevice);
+	//cudaMemcpy(d_T_tmp, h_T_tmp, grid_size*sizeof(double), cudaMemcpyHostToDevice);
 
 	int blocks = ceil((double)Px*Py/1000);
 	int threadsperblock = 1000;
 	printf("Running on %d blocks each with %d threads\n",blocks,threadsperblock);
-	//kernel<<<blocks,threadsperblock>>>(d_T,d_S);
+	kernel<<<blocks,threadsperblock>>>(d_T,d_T_tmp,d_S,grid_size,Px,Py);
 
 	cudaDeviceSynchronize();
+
+	cudaMemcpy(h_T, d_T, grid_size*sizeof(double), cudaMemcpyDeviceToHost);
+	for(int i=0; i<grid_size; i++) {
+		printf("%lf ", h_T[i]);
+		if(i%Px==Px-1)
+			printf("...\n");
+	}
 
 }
